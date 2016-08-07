d8abc171a374990f0574c5a4f15cf943d7193850
John Gill <j.gill0431@gmail.com>
2016-08-06 23:49:08 -0700

    complete boards 1 - 5

--- runner.rb
+++ runner.rb
@@ -12,30 +12,28 @@ require_relative 'sudoku'
 # Remember, the file has newline characters at the end of each line,
 # so we call String#chomp to remove them.
 
-# board_string = File.readlines('sudoku_puzzles.txt').first.chomp
+board_string = File.readlines('sudoku_puzzles.txt').first.chomp
 
-# solved_board = solve(board_string)
-# if solved?(solved_board)
-#   puts "The board was solved!"
-#   puts pretty_board(solved_board)
-# else
-#   puts "The board wasn't solved :("
-# end
+solved_board = solve(board_string)
+if solved?(solved_board)
+  puts "The board was solved!"
+  puts pretty_board(solved_board)
+else
+  puts "The board wasn't solved :("
+end
 
-string = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
-array = data_to_int(string)
-board = create_board(array)
-p zerod_board = loop_through_zeros(board)
-
-# zerod_board = loop_through_string(board)
+# string = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+# array = data_to_int(string)
+# board = create_board(array)
+# p zerod_board = loop_through_zeros(board)
 
 # zerod_board = loop_through_zeros(zerod_board)
 # zerod_board = loop_through_zeros(zerod_board)
-
 # p zerod_board
 
 
-# board = [[1, 0, 5, 8, 0, 2, 0, 0, 0],
+# board =           [
+#                  [1, 0, 5, 8, 0, 2, 0, 0, 0],
 #                  [0, 9, 0, 0, 7, 6, 4, 0, 5],
 #                  [2, 0, 0, 4, 0, 0, 8, 1, 9],
 #                  [0, 1, 9, 0, 0, 7, 3, 0, 6],
@@ -43,28 +41,9 @@ p zerod_board = loop_through_zeros(board)
 #                  [0, 0, 0, 0, 6, 1, 0, 5, 0],
 #                  [0, 0, 7, 6, 0, 0, 0, 3, 0],
 #                  [4, 3, 0, 0, 2, 0, 5, 0, 1],
-#                [6, 0, 0, 3, 0, 8, 9, 0, 0]]
+#                  [6, 0, 0, 3, 0, 8, 9, 0, 0]]
 
 # p loop_through_zeros(board)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # [[1, {1=>true, 2=>true, 3=>true, 4=>false, 5=>true, 6=>true, 7=>false, 8=>true, 9=>true}, 5, 8, 3, 2, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}],
 # [{1=>true, 2=>true, 3=>false, 4=>true, 5=>true, 6=>true, 7=>true, 8=>false, 9=>true}, 9, {1=>true, 2=>true, 3=>false, 4=>true, 5=>true, 6=>true, 7=>true, 8=>false, 9=>true}, 1, 7, 6, 4, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, 5],
@@ -75,3 +54,14 @@ p zerod_board = loop_through_zeros(board)
 #  [{1=>true, 2=>true, 3=>true, 4=>true, 5=>false, 6=>true, 7=>true, 8=>false, 9=>false}, {1=>true, 2=>false, 3=>true, 4=>true, 5=>false, 6=>true, 7=>true, 8=>false, 9=>true}, 7, 6, {1=>false, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>false}, {1=>true, 2=>true, 3=>true, 4=>true, 5=>false, 6=>true, 7=>true, 8=>true, 9=>false}, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, 3, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}],
 #  [4, 3, 8, 7, 2, 9, 5, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, 1],
 #  [6, {1=>true, 2=>false, 3=>true, 4=>true, 5=>false, 6=>true, 7=>true, 8=>true, 9=>true}, 1, 3, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, 8, 9, {1=>true, 2=>true, 3=>true, 4=>true, 5=>true, 6=>true, 7=>true, 8=>true, 9=>true}, 0]]
+
+
+[[1, 0, 5, 8, 0, 2, 0, 0, 0],
+                 [0, 9, 0, 1, 7, 6, 4, 2, 5],
+                 [2, 7, 0, 4, 0, 5, 8, 1, 9],
+                 [0, 1, 9, 0, 0, 7, 3, 0, 6],
+                 [7, 6, 2, 5, 8, 3, 1, 9, 4],
+                 [0, 0, 0, 0, 6, 1, 0, 5, 0],
+                 [0, 0, 7, 6, 0, 0, 2, 3, 8],
+                 [4, 3, 8, 0, 2, 9, 5, 0, 1],
+                 [6, 0, 1, 3, 0, 8, 9, 0, 7]]
--- sudoku.rb
+++ sudoku.rb
@@ -3,15 +3,6 @@
 # something representing a board after
 # your solver has tried to solve it.
 # How you represent your board is up to you!
-def solve(board_string)
-end
-
-# Returns a boolean indicating whether
-# or not the provided board is solved.
-# The input board will be in whatever
-# form `solve` returns.
-def solved?(board)
-end
 
 # Takes in a board in some form and
 # returns a _String_ that's well formatted
@@ -21,171 +12,262 @@ end
 def pretty_board(board)
 end
 
-def find_zero(board)
-  row = 0
-  column = 0
-  until row == 9 && column == 0
-    if board[row][column] == 0
-      return [row, column]
+def solve(string)
+board = create_board(string))
+i = 0
+    until solved?(board) == true
+      string_board = loop_through_zeros(board)
+      board = empty_to_zero(string_board)
+        if i > 5
+          return board
+        end
+      i += 1
     end
+    board
+end
 
-    if column == 8
-     column = 0
-     row += 1
-    else
-      column += 1
+  # takes board with strings and returns new board with 0's
+  def empty_to_zero(board)
+    row = 0
+    column = 0
+    until row == 9 && column == 0
+      if board[row][column] == ''
+        board[row][column] = 0
+      end
+      if column == 8
+       column = 0
+       row += 1
+      else
+        column += 1
+      end
     end
+    board
   end
-  false
-end
 
-def data_to_int(mixed_string)
-	mixed_string.chars.map!{ |item| item.to_i }
-end
-
-def create_board(array)
-	begin_idx = 0
-	end_idx = 8
-	board_array = []
-	until end_idx > 80
-	board_array << array[begin_idx..end_idx]
-	begin_idx += 9
-	end_idx += 9
-	end
-	board_array
-end
-
-
-
-def box_identifier(location_array)
-  row, column = location_array
-
-  box_row1 = (0..2)
-  box_row2 = (3..5)
-  box_row3 = (6..8)
-
-  box_column1 = (0..2)
-  box_column2 = (3..5)
-  box_column3 = (6..8)
-
-  if box_row1.include?(row) && box_column1.include?(column)
-    return 1
-  elsif box_row1.include?(row) && box_column2.include?(column)
-    return 2
-  elsif box_row1.include?(row) && box_column3.include?(column)
-    return 3
-  elsif box_row2.include?(row) && box_column1.include?(column)
-    return 4
-  elsif box_row2.include?(row) && box_column2.include?(column)
-    return 5
-  elsif box_row2.include?(row) && box_column3.include?(column)
-    return 6
-  elsif box_row3.include?(row) && box_column1.include?(column)
-    return 7
-  elsif box_row3.include?(row) && box_column2.include?(column)
-    return 8
-  elsif box_row3.include?(row) && box_column3.include?(column)
-    return 9
+  # Takes String Reutrns BOARD!
+  def create_board(string)
+    array = data_to_int(string)
+  	begin_idx = 0
+  	end_idx = 8
+  	board_array = []
+  	until end_idx > 80
+  	board_array << array[begin_idx..end_idx]
+  	begin_idx += 9
+  	end_idx += 9
+  	end
+  	board_array
   end
-end
 
-def box_start_location(boxnum)
- case boxnum
-  when 1
-    return [0, 0]
-  when 2
-    return [0, 3]
-  when 3
-    return [0, 6]
-  when 4
-    return [3, 0]
-  when 5
-    return [3, 3]
-  when 6
-    return [3, 6]
-  when 7
-    return [6, 0]
-  when 8
-    return [6, 3]
-  when 9
-    return [6, 6]
-  end
-end
-
-def check_row(board, row_idx, num_search)
-  board[row_idx].include?(num_search)
-end
-
-def check_column(board, column_idx, num_search)
- check_row(board.transpose, column_idx, num_search)
-end
-
-def check_box(board, cell_coordinates, num_search)
-  row, column = box_start_location(box_identifier(cell_coordinates))
-  stop_row = row + 2
-  stop_column = column + 2
-  until  ow == stop_row && column == stop_column
-    if num_search == board[row][column]
-      return true
-    end
-
-    if column == stop_column
-     column = 0
-     row += 1
-    else
-      column += 1
-    end
-  end
-    false
-end
-
-def cell_check(board, start_array, num_search)
-  row_idx, column_idx = start_array
-  check_r = check_row(board, row_idx, num_search)
-  check_c = check_column(board, column_idx, num_search)
-  check_b = check_box(board, start_array, num_search)
-  if check_r || check_c || check_b
-    true
-  else
-    false
-  end
-end
-
-def cell_conflicts(board, start_location)
-  z = 1
-  cell = {}
-  until z == 10
-    conflict = cell_check(board, start_location, z)
-    cell[z] = conflict
-    z += 1
-  end
-cell
-end
+        def data_to_int(mixed_string)
+          mixed_string.chars.map!{ |item| item.to_i }
+        end
 
 
-def false_count(hash)
-  conflict_collection = []
-  hash.each_pair {|num, conflict| conflict_collection << conflict }
-  conflict_collection.count(false)
-end
-
-def loop_through_zeros(board)
-  cell_location = find_zero(board)
-  row, column = cell_location
-  until row == false
-   # p cell_location
-    hash = cell_conflicts(board, cell_location)
-    if false_count(hash) <= 1
-      num = hash.key(false)
-      board[row][column] = num
-    else
-      board[row][column] = ''
-    end
+  # Takes board and returns blank adjusted to '' and filled in solved place!!!
+  def loop_through_zeros(board)
     cell_location = find_zero(board)
     row, column = cell_location
+    until row == false
+     # p cell_location
+      hash = cell_conflicts(board, cell_location)
+      if false_count(hash) <= 1
+        num = hash.key(false)
+        board[row][column] = num
+      else
+        board[row][column] = ''
+      end
+      cell_location = find_zero(board)
+      row, column = cell_location
+    end
+    board
   end
-  board
+
+       def find_zero(board)
+          row = 0
+          column = 0
+          until row == 9 && column == 0
+            if board[row][column] == 0
+              return [row, column]
+            end
+
+            if column == 8
+             column = 0
+             row += 1
+            else
+              column += 1
+            end
+          end
+          false
+        end
+
+        def false_count(hash)
+          conflict_collection = []
+          hash.each_pair {|num, conflict| conflict_collection << conflict }
+          conflict_collection.count(false)
+        end
+
+          # Returns HASH with conflicts, still need to do false count
+        def cell_conflicts(board, start_location)
+          z = 1
+          cell = {}
+          until z == 10
+            conflict = cell_check(board, start_location, z)
+            cell[z] = conflict
+            z += 1
+          end
+        cell
+        end
+
+        def cell_conflicts(board, start_location)
+          z = 1
+          cell = {}
+          until z == 10
+            conflict = cell_check(board, start_location, z)
+            cell[z] = conflict
+            z += 1
+          end
+        cell
+        end
+
+            def cell_check(board, start_array, num_search)
+              row_idx, column_idx = start_array
+              check_r = check_row(board, row_idx, num_search)
+              check_c = check_column(board, column_idx, num_search)
+              check_b = check_box(board, start_array, num_search)
+              if check_r || check_c || check_b
+                true
+              else
+                false
+              end
+            end
+
+                    def check_row(board, row_idx, num_search)
+                      board[row_idx].include?(num_search)
+                    end
+
+                    def check_column(board, column_idx, num_search)
+                     check_row(board.transpose, column_idx, num_search)
+                    end
+
+
+                    def check_box(board, cell_coordinates, num_search)
+                      row, column = box_start_location(box_identifier(cell_coordinates))
+                      start_column = column
+                      stop_row = row + 2
+                      stop_column = column + 2
+                      array = []
+                      until  row == stop_row && column == stop_column
+                         # array << board[row][column]
+                        if num_search == board[row][column]
+                          # p array
+                          return true
+                        end
+                        if column == stop_column
+                         column = start_column
+                         row += 1
+                        else
+                          column += 1
+                        end
+                      end
+                        # p array
+                        false
+                    end
+                                  def box_identifier(location_array)
+                                    row, column = location_array
+
+                                    box_row1 = (0..2)
+                                    box_row2 = (3..5)
+                                    box_row3 = (6..8)
+
+                                    box_column1 = (0..2)
+                                    box_column2 = (3..5)
+                                    box_column3 = (6..8)
+
+                                    if box_row1.include?(row) && box_column1.include?(column)
+                                      return 1
+                                    elsif box_row1.include?(row) && box_column2.include?(column)
+                                      return 2
+                                    elsif box_row1.include?(row) && box_column3.include?(column)
+                                      return 3
+                                    elsif box_row2.include?(row) && box_column1.include?(column)
+                                      return 4
+                                    elsif box_row2.include?(row) && box_column2.include?(column)
+                                      return 5
+                                    elsif box_row2.include?(row) && box_column3.include?(column)
+                                      return 6
+                                    elsif box_row3.include?(row) && box_column1.include?(column)
+                                      return 7
+                                    elsif box_row3.include?(row) && box_column2.include?(column)
+                                      return 8
+                                    elsif box_row3.include?(row) && box_column3.include?(column)
+                                      return 9
+                                    end
+                                  end
+
+
+
+                                  def box_start_location(boxnum)
+                                   case boxnum
+                                    when 1
+                                      return [0, 0]
+                                    when 2
+                                      return [0, 3]
+                                    when 3
+                                      return [0, 6]
+                                    when 4
+                                      return [3, 0]
+                                    when 5
+                                      return [3, 3]
+                                    when 6
+                                      return [3, 6]
+                                    when 7
+                                      return [6, 0]
+                                    when 8
+                                      return [6, 3]
+                                    when 9
+                                      return [6, 6]
+                                    end
+                                  end
+
+
+# True, puzzle is solved.
+# False puzzle not solved
+def solved?(board)
+  potential = []
+  board.map do |item|
+    potential << !item.include?('')
+  end
+  potential.include?(false)
 end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def loop(board)
 #   new_board = []
@@ -194,24 +276,3 @@ end
 #   end
 #   p result
 # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
